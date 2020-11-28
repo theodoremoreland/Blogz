fa57eb3ae73a3db4e21d960ee97d56cbdcc826f3
Theodore Moreland <theodoremoreland@yahoo.com>
2020-11-28 01:01:09 -0600

    Corrected template reference in SignUp view and updated templates

--- application/blueprints/SignUp/views.py
+++ application/blueprints/SignUp/views.py
@@ -5,7 +5,7 @@ from ...models import db, Users
 signup = Blueprint(
     'signup',
     __name__, 
-    template_folder='...templates',
+    template_folder='templates',
     static_folder='static'
     )
 
