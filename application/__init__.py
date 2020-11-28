ebacc8da849685abc1989ac0eae6651b18df4602
Theodore Moreland <theodoremoreland@yahoo.com>
2020-11-27 19:20:42 -0600

    Bugfix: attempted to resolve SQL Queue error by replacing db instantiation with import

--- application/__init__.py
+++ application/__init__.py
@@ -1,10 +1,12 @@
-  
 # Third party
 from flask import Flask, redirect, request, session
 from flask_sqlalchemy import SQLAlchemy
 
-# Custom
+# Models
+from .models import db
 from .models import Users, BlogPosts
+
+# Blueprints
 from .blueprints.Auth.views import auth
 from .blueprints.Blog.views import blog
 from .blueprints.CreateBlogPost.views import create_blog_post
@@ -20,7 +22,6 @@ app.register_blueprint(blog)
 app.register_blueprint(create_blog_post)
 app.register_blueprint(home)
 app.register_blueprint(signup)
-db = SQLAlchemy()
 db.init_app(app)
 
 
