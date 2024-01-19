0923099569de2553446f3282009a9ae1178627ef
theodoremoreland <theodoremoreland@yahoo.com>
2024-01-19 16:14:49 -0600

    root now redirects to user blog if user is logged in

--- application/blueprints/Root/views.py
+++ application/blueprints/Root/views.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, render_template, request, redirect
+from flask import Blueprint, render_template, redirect, session
 
 from modules.logger import logger
+from db.models import Users
 
 root = Blueprint("root", __name__, template_folder="templates", static_folder="static")
 
@@ -8,12 +9,16 @@ root = Blueprint("root", __name__, template_folder="templates", static_folder="s
 @root.route("/")
 def handle_root_request():
     try:
-        user_id = request.args.get("id")
+        if "username" in session:
+            logger.info(
+                f"User {session['username']} already logged in. Attempting to redirect to blog page..."
+            )
 
-        logger.info(f"Handling request @ root with user_id: {user_id}")
+            user = Users.query.filter_by(username=session["username"]).first()
 
-        if user_id:
-            return redirect(f"/blog?user={user_id}")
+            logger.info(f"Handling request @ root with user_id: {user.id}")
+
+            return redirect(f"/blog?user={user.id}")
         else:
             logger.info(f"User ID not found, redirecting to bloggers page")
 
