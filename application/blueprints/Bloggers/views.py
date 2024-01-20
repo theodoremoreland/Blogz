1bb5475a5654fb02cb6da67ef32249c5990455f9
theodoremoreland <theodoremoreland@yahoo.com>
2024-01-20 15:38:25 -0600

    added post count to bloggers view

--- application/blueprints/Bloggers/views.py
+++ application/blueprints/Bloggers/views.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template
 
-from db.models import Users
+from db.models import Users, BlogPosts
 from modules.logger import logger
 
 bloggers = Blueprint(
@@ -12,8 +12,15 @@ bloggers = Blueprint(
 def render_bloggers_page():
     try:
         users = Users.query.all()
+        post_count_by_user = {}
 
-        return render_template("bloggers.html", users_list=users)
+        for user in users:
+            post_count = len(user.blog_posts)
+            post_count_by_user[user.id] = post_count
+
+        return render_template(
+            "bloggers.html", users_list=users, post_count_by_user=post_count_by_user
+        )
     except Exception as e:
         logger.exception(f"Error rendering home page: {e}")
 
