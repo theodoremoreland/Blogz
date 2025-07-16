bfd57ef1f7067e597abafa0ec5852afb5d752215
Theodore Moreland <theodoremoreland@yahoo.com>
2025-07-15 21:07:25 -0500

    fixed img validation bug

--- application/utils/validate_image_url.py
+++ application/utils/validate_image_url.py
@@ -1,8 +1,11 @@
 def validate_image_url(url):
+    is_valid = True
+
     if url == "":
-        return True
-    if url.startswith("http://") or url.startswith("https://"):
-        return True
-    if url.endswith(".jpg") or url.endswith(".jpeg") or url.endswith(".png"):
-        return True
-    return False
+        is_valid = False
+    if url.startswith("http://") == False and url.startswith("https://") == False:
+        is_valid = False
+    if url.endswith(".jpg") == False and url.endswith(".jpeg") == False and url.endswith(".png") == False:
+        is_valid = False
+
+    return is_valid
